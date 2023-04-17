585435077d3f844b38248047f5f121967bb2627a
Maksim Troshin <mstroshin@gmail.com>
2023-04-17 16:48:15 +0300

    Initial Commit

---
+++ UniteaTestProject/UniteaTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniteaTestProjectApp.swift
+//  UniteaTestProject
+//
+//  Created by Maksim Troshin on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct UniteaTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
